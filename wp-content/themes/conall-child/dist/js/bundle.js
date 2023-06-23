bd5436225402d8eeb870a8c93a905aedf8fc3957
Max <maxsevostianov@192.168.1.3>
2023-06-23 16:34:19 -0400

    moa section styles 2

--- wp-content/themes/conall-child/dist/js/bundle.js
+++ wp-content/themes/conall-child/dist/js/bundle.js
@@ -1,1 +1,19 @@
-(()=>{var n;(n=jQuery)(document).ready((function(){n(".map-ping, #close-map-icon").on("click",(function(){n("#map-popup_section").toggle()}))}))})();
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/js/bundle.js ***!
+  \**************************/
+(function ($) {
+  $(document).ready(function () {
+    $('.map-ping, #close-map-icon').on('click', function () {
+      $('#map-popup_section').toggle();
+    });
+
+    /*$('#close-map-icon').on('click', function() {
+      $('#map-popup_section').toggle();
+    });*/
+  });
+})(jQuery);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsQ0FBQyxVQUFVQSxDQUFDLEVBQUU7RUFDWkEsQ0FBQyxDQUFDQyxRQUFRLENBQUMsQ0FBQ0MsS0FBSyxDQUFDLFlBQVk7SUFFaENGLENBQUMsQ0FBQyw0QkFBNEIsQ0FBQyxDQUFDRyxFQUFFLENBQUMsT0FBTyxFQUFFLFlBQVc7TUFDckRILENBQUMsQ0FBQyxvQkFBb0IsQ0FBQyxDQUFDSSxNQUFNLEVBQUU7SUFDbEMsQ0FBQyxDQUFDOztJQUVGO0FBQ0E7QUFDQTtFQUVFLENBQUMsQ0FBQztBQUNKLENBQUMsRUFBRUMsTUFBTSxDQUFDLEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90aGVhZ2VuY3luZXR3b3JrLy4vc3JjL2pzL2J1bmRsZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIoZnVuY3Rpb24gKCQpIHtcbiAgJChkb2N1bWVudCkucmVhZHkoZnVuY3Rpb24gKCkge1xuXG4kKCcubWFwLXBpbmcsICNjbG9zZS1tYXAtaWNvbicpLm9uKCdjbGljaycsIGZ1bmN0aW9uKCkge1xuICAkKCcjbWFwLXBvcHVwX3NlY3Rpb24nKS50b2dnbGUoKTtcbn0pO1xuXG4vKiQoJyNjbG9zZS1tYXAtaWNvbicpLm9uKCdjbGljaycsIGZ1bmN0aW9uKCkge1xuICAkKCcjbWFwLXBvcHVwX3NlY3Rpb24nKS50b2dnbGUoKTtcbn0pOyovXG5cbiAgfSk7XG59KShqUXVlcnkpIl0sIm5hbWVzIjpbIiQiLCJkb2N1bWVudCIsInJlYWR5Iiwib24iLCJ0b2dnbGUiLCJqUXVlcnkiXSwic291cmNlUm9vdCI6IiJ9
