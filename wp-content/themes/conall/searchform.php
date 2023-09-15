f9f51a1aaf4341426f85f81ce9f39b27c5686269
billiemead <billie@billiemead.com>
2023-09-15 14:30:21 -0400

    change header font size & search

--- wp-content/themes/conall/searchform.php
+++ wp-content/themes/conall/searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-	<div><label class="screen-reader-text" for="s"><?php esc_html_e( 'Search for:', 'conall' ); ?></label>
+	<div><label class="screen-reader-text" for="s"><?php esc_html_e( 'Search:', 'conall' ); ?></label>
 		<input type="text" value="" placeholder="<?php esc_attr_e('Search', 'conall'); ?>" name="s" id="s" />
 		<input type="submit" id="searchsubmit" value="&#x55;" />
 	</div>
