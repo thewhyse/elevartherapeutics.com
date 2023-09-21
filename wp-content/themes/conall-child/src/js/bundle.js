dea85fac5b07d643a89aecd3a76c7d31664b56ef
billiemead <billie@billiemead.com>
2023-09-21 17:48:13 -0400

    final ESMO sprint push

--- wp-content/themes/conall-child/src/js/bundle.js
+++ wp-content/themes/conall-child/src/js/bundle.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function ($) {
 
-    $('.map-ping, #close-map-icon-2023').on('click', function () {
+    $('.map-ping, #close-map-icon').on('click', function () {
         $('#map_popup_section_2023').toggle();
     });
 
