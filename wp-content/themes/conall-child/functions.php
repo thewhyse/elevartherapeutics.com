9606e21d0cf64a7afca921573f2e45737907ef2d
billiemead <billie@billiemead.com>
2023-09-21 11:56:37 -0400

    remove corp updates popup

--- wp-content/themes/conall-child/functions.php
+++ wp-content/themes/conall-child/functions.php
@@ -17,7 +17,7 @@ function elevar_assets() {
     wp_register_style('elevar-stylesheet', get_theme_file_uri() . '/dist/css/bundle.css', array(), '1.0.0', 'all');
     wp_enqueue_style('elevar-stylesheet');
     wp_enqueue_script('elevar_js', get_theme_file_uri() . '/dist/js/bundle.js', array('jquery'), '1.0.0', true);
-    wp_enqueue_script('custom_js', get_stylesheet_directory_uri() . '/elevar-scripts.js', array(), '1.0.0', true);
+    // wp_enqueue_script('custom_js', get_stylesheet_directory_uri() . '/elevar-scripts.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'elevar_assets', 99);
 
