79d63a45a7cd8e7d55e4fc3c50fc2f4c255a77c0
billiemead <billie@billiemead.com>
2023-09-24 12:30:51 -0400

    fix leadership page spacing - section by section for 10 breakpoints

---
+++ wp-content/themes/conall-child/footer.php
@@ -0,0 +1,34 @@
+<div class="wp-bottom-menu elevar-mobile-bar" id="wp-bottom-menu">
+    <a href="" class="wp-bottom-menu-item">
+        <div class="wp-bottom-menu-icon-wrapper">
+            <i class="wp-bottom-menu-item-icons fa fa fa-heartbeat"></i>
+        </div>
+        <span>Rivoceranib</span>
+    </a>
+    <a href="" class="wp-bottom-menu-item">
+        <div class="wp-bottom-menu-icon-wrapper">
+            <i class="wp-bottom-menu-item-icons fa fa fa-bullhorn"></i>
+        </div>
+        <span>Press Releases</span>
+    </a>
+    <a href="" class="wp-bottom-menu-item">
+        <div class="wp-bottom-menu-icon-wrapper">
+            <i class="wp-bottom-menu-item-icons fa fa fa-usd"></i>
+        </div>
+        <span>Investors</span>
+    </a>
+    <a href="" class="wp-bottom-menu-item">
+        <div class="wp-bottom-menu-icon-wrapper">
+            <i class="wp-bottom-menu-item-icons fa fa fa-ticket"></i>
+        </div>
+        <span>Events</span>
+    </a>
+    <div title="Careers" class="wp-bottom-menu-item wp-bottom-menu-nav-trigger">
+        <div class="wp-bottom-menu-icon-wrapper">
+            <i class="wp-bottom-menu-item-icons fa fa fa-graduation-cap"></i>
+        </div>
+        <span>Careers</span>
+    </div>
+</div>
+
+<?php conall_edge_get_footer(); ?>
