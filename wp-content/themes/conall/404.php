8b4fbf3129bb41f751b08c7e5cddd7eaa2f4cafd
billiemead <billie@billiemead.com>
2023-10-09 15:20:07 -0400

    penbanner. 404 & mainteance pages

--- wp-content/themes/conall/404.php
+++ wp-content/themes/conall/404.php
@@ -1,63 +1,15 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <?php
-    /**
-     * conall_edge_header_meta hook
-     *
-     * @see conall_edge_header_meta() - hooked with 10
-     * @see edgt_user_scalable_meta() - hooked with 10
-     */
-    do_action('conall_edge_header_meta');
-    
-    wp_head(); ?>
-</head>
-<body <?php body_class();?> itemscope itemtype="http://schema.org/WebPage">
-	<?php if(conall_edge_options()->getOptionValue('smooth_page_transitions') == "yes") { ?>
-		<div class="edgtf-smooth-transition-loader edgtf-mimic-ajax">
-		    <div class="edgtf-st-loader">
-		        <div class="edgtf-st-loader1">
-		            <?php conall_edge_loading_spinners(); ?>
-		        </div>
-		    </div>
-		</div>
-	<?php } ?>
-	<div class="edgtf-wrapper edgtf-404-page">
-	    <div class="edgtf-wrapper-inner">
-			<div class="edgtf-content" <?php conall_edge_content_elem_style_attr(); ?>>
-	            <div class="edgtf-content-inner">
-					<div class="edgtf-page-not-found">
-						<h1><span>
-							<?php if(conall_edge_options()->getOptionValue('404_title')){
-								echo esc_html(conall_edge_options()->getOptionValue('404_title'));
-							} else {
-								esc_html_e('404', 'conall');
-							} ?>
-						</span></h1>
-						<h4><span>
-							<?php if(conall_edge_options()->getOptionValue('404_subtitle')){
-								echo esc_html(conall_edge_options()->getOptionValue('404_subtitle'));
-							} else {
-								esc_html_e('Page not found', 'conall');
-							} ?>
-						</span></h4>
-						<?php
-							$params = array();
-							if (conall_edge_options()->getOptionValue('404_back_to_home')){
-								$params['text'] = conall_edge_options()->getOptionValue('404_back_to_home');
-							} else {
-								$params['text'] = "BACK TO HOME";
-							}
-							$params['link'] = esc_url(home_url('/'));
-							$params['target'] = '_self';
-							$params['type'] = 'solid';
-							$params['size'] = 'huge';
-						echo conall_edge_execute_shortcode('edgtf_button',$params);?>
-					</div>
-				</div>	
-			</div>
-		</div>
-	</div>		
-	<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_header(); ?>
+
+<div class="edgtf-container edgtf-404-page">
+    <?php do_action('conall_edge_after_container_open'); ?>
+    <div class="edgtf-container-inner clearfix">
+        <div class="edgtf-page-not-found">
+            <h1 class=""><span>SORRY, THAT PAGE DOES NOT EXIST</span></h1>
+            <h4 class=""><span>Lets get you back on track</span></h4>
+            <a class="edgtf-btn" href="<?php echo get_home_url(); ?>">Back to home</a>
+        </div>
+    </div>
+    <?php do_action('conall_edge_before_container_close'); ?>
+</div>
+
+<?php get_footer(); ?>
