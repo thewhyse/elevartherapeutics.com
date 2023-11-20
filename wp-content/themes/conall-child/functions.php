93f89fc990def3993fa2ddbabf0707e372ffb0bc
billiemead <bmead@mjhlifesciences.com>
2023-11-20 14:32:38 -0500

    New ESMO page styling add new WADE photos

--- wp-content/themes/conall-child/functions.php
+++ wp-content/themes/conall-child/functions.php
@@ -201,7 +201,43 @@ function marker_io_pubs()
             }(window, document);
         </script>
 
-<?php
+    <?php
     }
 }
 add_action('wp_head', 'marker_io_pubs');
+
+function marker_io_leadership()
+{
+    if (is_page('elevar-leadership')) {
+    ?>
+        <script>
+            window.markerConfig = {
+                project: '655bad2e192afe7ca2b07bce',
+                source: 'snippet'
+            };
+
+            ! function(e, r, a) {
+                if (!e.__Marker) {
+                    e.__Marker = {};
+                    var t = [],
+                        n = {
+                            __cs: t
+                        };
+                    ["show", "hide", "isVisible", "capture", "cancelCapture", "unload", "reload", "isExtensionInstalled", "setReporter", "setCustomData", "on", "off"].forEach(function(e) {
+                        n[e] = function() {
+                            var r = Array.prototype.slice.call(arguments);
+                            r.unshift(e), t.push(r)
+                        }
+                    }), e.Marker = n;
+                    var s = r.createElement("script");
+                    s.async = 1, s.src = "https://edge.marker.io/latest/shim.js";
+                    var i = r.getElementsByTagName("script")[0];
+                    i.parentNode.insertBefore(s, i)
+                }
+            }(window, document);
+        </script>
+
+<?php
+    }
+}
+add_action('wp_head', 'marker_io_leadership');
